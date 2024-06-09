5d20b0a86f064c9a090c0203917868ef51c529a5
raja_choudhary <raja@choudhary.com>
2024-06-09 09:50:44 +0530

    here navbar herf fixed

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -80,12 +80,12 @@ export default function index({ children }) {
                           ? adminNavOptions.map((item) => (
                               <p
                                 key={item.name}
-                                href={item.href}
+                                onClick={()=> router.push(item.href)}
                                 className={classNames(
                                   pathName === item.href
                                     ? "bg-gray-900 text-purple-300"
                                     : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                                  "rounded-md px-3 py-2 text-sm font-medium"
+                                  "rounded-md px-3 py-2 text-sm font-medium cursor-pointer"
                                 )}
                                 aria-current={
                                   pathName === item.href ? "page" : undefined
